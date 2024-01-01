f2bc5928a451910f5abde25a3402fdb92329f949
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-01 14:07:39 +0200

    fixed labels

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function getNameOne() {
     const x = document.getElementById("oneInputId");
     const z = document.getElementById("oneDiv");    
-    var nameTwo = x.value;
-    var fullNameOne ="<label id='labelOne'>${nameOne}</label>";
+    var nameOne = x.value;
+    var fullNameOne ="<label id='labelOne'>" + nameOne + "</label>";
     console.log(fullNameOne);
     document.getElementById('labelOne').outerHTML = fullNameOne;
 }
@@ -12,7 +12,7 @@ function getNameTwo() {
     const x = document.getElementById("twoInputId");    
     const z = document.getElementById("twoDiv");
     var nameTwo = x.value;
-    var fullNameTwo ='<label id="labelTwo">${nameTwo}</label>';
+    var fullNameTwo ="<label id='labelTwo'>" + nameTwo + "</label>';
     console.log(fullNameTwo);    
     document.getElementById('labelTwo').outerHTML = fullNameTwo;     
 }
