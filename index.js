0abe67728a4cdce1bcb3aaa7cd571f58886a3c0a
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-01 17:22:34 +0200

    fixed small things

--- index.js
+++ index.js
@@ -40,12 +40,14 @@ function addButtons(){
         eButtonOne.outerHTML = button1New;
         eButtonOne.classList.add("rollButton1");  
         eButtonTwo.outerHTML = button2New;
-        eButtonTwo.classList.add("rollButton2");      
+        eButtonTwo.classList.add("rollButton2");
+        buttonsAvailable = true;      
     } else {
         eButtonOne.outerHTML = button1Old;
         eButtonOne.classList.remove("rollButton1");  
         eButtonTwo.outerHTML = button2Old;
-        eButtonTwo.classList.remove("rollButton2");            
+        eButtonTwo.classList.remove("rollButton2");
+        buttonsAvailable = false;            
     }
     console.log(button1New);
     console.log(button2New);        
