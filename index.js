6a90b0d4f9ff4f16e24c4be895ae980b24402076
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-01 16:52:48 +0200

    quick upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let nameOneGiven = false;
 let nameTwoGiven = false;
+const origButtonOne = document.getElementById("labelOne");
+const origButtonTwo = document.getElementById("labelTwo");
+var button1Old="<label id='labelOne'>Name1</label>";
+var button2Old="<label id='labelTwo'>Name2</label>";
+var button1New="<button onclick = 'rollDice1()'>Roll Dice One</button>"
+var button2New="<button onclick = 'rollDice2()'>Roll Dice Two</button>"
 
 
 function getNameOne() {
@@ -10,7 +16,7 @@ function getNameOne() {
     document.getElementById('labelOne').outerHTML = fullNameOne;
     nameOneGiven = true;
     if (nameTwoGiven === true) {
-        addButton();
+        addButtons();
     }
 }
 
@@ -22,13 +28,21 @@ function getNameTwo() {
     document.getElementById('labelTwo').outerHTML = fullNameTwo;
     nameTwoGiven = true;
     if (nameOneGiven === true) {
-        addButton(); 
+        addButtons(""); 
     }    
 }
 
-function addButton(){
-    const x = document.getElementById("oneDiv");
-    console.log(x);
-    x.classList.add("rollButton");
+function addButtons(){
+    const inp1 = origButtonOne;
+    const inp2 = origButtonTwo;    
+    console.log(inp1);
+    console.log(inp2);    
+    document.getElementById('labelOne').outerHTML = button1New;
+    document.getElementById('labelTwo').outerHTML = button2New;    
+    x.classList.add("rollButton");    
     // oneInputId
+}
+
+function rollDice1(){
+     // oneInputId   
 }
